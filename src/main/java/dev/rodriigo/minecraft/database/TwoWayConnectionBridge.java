a99f03469cf75d457829546e85ed8c65f36d8331
rodri <rodrigoe8228@gmail.com>
2024-01-24 21:55:35 -0500

    Database driver not working properly. Updated security for queries

---
+++ src/main/java/dev/rodriigo/minecraft/database/TwoWayConnectionBridge.java
@@ -0,0 +1,245 @@
+package dev.rodriigo.minecraft.database;
+
+import dev.rodriigo.minecraft.BackendPlugin;
+import dev.rodriigo.minecraft.util.StringUtil;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.sql.Connection;
+import java.util.*;
+
+public class TwoWayConnectionBridge implements NormalizedDatabaseBridge {
+    // This class is used to create a connection to the database
+    // This will support both SQL and MongoDB and will load the correct driver
+    // based on a configuration file
+
+    Connection connection;
+    MongoDBConnector mongoDB;
+    BackendPlugin backendPlugin = BackendPlugin.getInstance();
+    NoSQLConnectionBridge noSQLConnectionBridge;
+    SQLConnectionBridge sqlConnectionBridge;
+
+
+    public TwoWayConnectionBridge(File configFile) throws Exception {
+        // Load the configuration file
+        // This will be used to determine which driver to use
+
+        final YamlConfiguration config = new YamlConfiguration();
+        config.load(configFile);
+
+        String driver = config.getString("storage.driver");
+        String database = config.getString("storage.database");
+        if (driver == null) {
+            throw new Exception("Database Driver is null into the configuration file. Please check the file.");
+        }
+
+        if (database == null) {
+            throw new Exception("Database name is null into the configuration file. Please check the file.");
+        }
+
+        if (driver.equalsIgnoreCase("mongodb")) {
+            // Create a connection to MongoDB
+            String uri = config.getString("storage.uri");
+            if (uri == null) {
+                throw new Exception("MongoDB URI is null into the configuration file. Please check the file.");
+            }
+            mongoDB = new MongoDBConnector(uri);
+            mongoDB.setDatabase(database);
+
+            noSQLConnectionBridge = new NoSQLConnectionBridge(mongoDB);
+        } else if (driver.equalsIgnoreCase("sqlite")) {
+            // Create a connection to SQLite
+            connection = new RelationalBridge(
+                    backendPlugin.getDataFolder()
+                            .toPath()
+                            .resolve(database)
+                            .toString()
+            ).getConnection();
+
+            sqlConnectionBridge = new SQLConnectionBridge(connection);
+        } else if (driver.equalsIgnoreCase("mysql")) {
+            // Create a connection to MySQL
+            String host = config.getString("storage.host");
+            int port = config.getInt("storage.port");
+            String username = config.getString("storage.username");
+            String password = config.getString("storage.password");
+            if (host == null) {
+                throw new Exception("MySQL Host is null into the configuration file. Please check the file.");
+            }
+            if (username == null) {
+                throw new Exception("MySQL Username is null into the configuration file. Please check the file.");
+            }
+            if (password == null) {
+                throw new Exception("MySQL Password is null into the configuration file. Please check the file.");
+            }
+            connection = new RelationalBridge(
+                    host,
+                    port,
+                    database,
+                    username,
+                    password
+            ).getConnection();
+
+            sqlConnectionBridge = new SQLConnectionBridge(connection);
+        } else {
+            throw new Exception("Database Driver is not supported. Please check the file.");
+        }
+    }
+
+    @Override
+    public List<Map<String, Object>> findFrom(String table, Map<String, Object> query, int... limit) {
+        try {
+            if (isTableNameInvalid(table)) {
+                throw new IllegalArgumentException("Invalid table name: " + table);
+            }
+
+            // Using a map provides us a more flexible query for both SQL and MongoDB
+            // Check the service using
+            final int queryLimit = limit.length > 0 ? Math.max(limit[0], 1) : 1;
+
+
+            if (sqlConnectionBridge != null) {
+                return sqlConnectionBridge.findFrom(table, query, queryLimit);
+            } else if (noSQLConnectionBridge != null) {
+                return noSQLConnectionBridge.findFrom(table, query, queryLimit);
+            } else {
+                throw new Exception("Connection is null. Please check the file.");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<Map<String, Object>> findFrom(String table, Map<String, Object> query) {
+        return findFrom(table, query, 1);
+    }
+
+    @Override
+    public List<Map<String, Object>> findOne(String table, Map<String, Object> query) {
+        // Recycle the same method but with a limit of 1
+        return findFrom(table, query);
+    }
+
+    @Override
+    public void updateFor(String table, Map<String, Object> query, Map<String, Object> update) {
+        try {
+            if (isTableNameInvalid(table)) {
+                throw new IllegalArgumentException("Invalid table name: " + table);
+            }
+            // Check the service using
+
+            if (sqlConnectionBridge != null) {
+                sqlConnectionBridge.updateFor(table, query, update);
+            } else if (noSQLConnectionBridge != null) {
+                noSQLConnectionBridge.updateFor(table, query, update);
+            } else {
+                throw new Exception("Connection is null. Please check the file.");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void deleteOne(String table, Map<String, Object> query) {
+        deleteFor(table, query);
+    }
+
+    @Override
+    public void deleteFor(String table, Map<String, Object> query, int... limit) {
+        try {
+            if (isTableNameInvalid(table)) {
+                throw new IllegalArgumentException("Invalid table name: " + table);
+            }
+
+            // Check the service using
+            final int queryLimit = limit.length > 0 ? Math.max(limit[0], 1) : 1;
+
+            if (sqlConnectionBridge != null) {
+                sqlConnectionBridge.deleteFor(table, query, queryLimit);
+            } else if (noSQLConnectionBridge != null) {
+                noSQLConnectionBridge.deleteFor(table, query);
+            } else {
+                throw new Exception("Connection is null. Please check the file.");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void deleteFor(String table, Map<String, Object> query) {
+        deleteFor(table, query, 1);
+    }
+
+    @Override
+    public void insertInto(String table, Map<String, Object> query) {
+        try {
+            if (isTableNameInvalid(table)) {
+                throw new IllegalArgumentException("Invalid table name: " + table);
+            }
+
+            // Check the service using
+
+            if (sqlConnectionBridge != null) {
+                sqlConnectionBridge.insertInto(table, query);
+            } else if (noSQLConnectionBridge != null) {
+                noSQLConnectionBridge.insertInto(table, query);
+            } else {
+                throw new Exception("Connection is null. Please check the file.");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void insertInto(String table, Iterable<Map<String, Object>> query) {
+        query.forEach(a -> insertInto(table, a));
+    }
+
+    @Override
+    public void dropTable(String table) {
+        try {
+            if (isTableNameInvalid(table)) {
+                throw new IllegalArgumentException("Invalid table name: " + table);
+            }
+
+            // Check the service using
+            if (sqlConnectionBridge != null) {
+                sqlConnectionBridge.dropTable(table);
+            } else if (noSQLConnectionBridge != null) {
+                noSQLConnectionBridge.dropTable(table);
+            } else {
+                throw new Exception("Connection is null. Please check the file.");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void dropDatabase(String table) {
+        try {
+            if (isTableNameInvalid(table)) {
+                throw new IllegalArgumentException("Invalid database name: " + table);
+            }
+
+            // Check the service using
+            if (sqlConnectionBridge != null) {
+                sqlConnectionBridge.dropDatabase(table);
+            } else if (noSQLConnectionBridge != null) {
+                noSQLConnectionBridge.dropDatabase(table);
+            } else {
+                throw new Exception("Connection is null. Please check the file.");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    boolean isTableNameInvalid(String table) {
+        return !StringUtil.strictMatches(table, "^[a-zA-Z_][a-zA-Z0-9_]*$");
+    }
+}
