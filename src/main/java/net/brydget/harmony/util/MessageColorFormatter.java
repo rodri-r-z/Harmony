36242ae1d1256490b498f9b468271e811fa45591
rodri <rodrigoe8228@gmail.com>
2024-01-29 21:28:43 -0500

    Add stuff to MessageColorFormatter

--- src/main/java/net/brydget/harmony/util/MessageColorFormatter.java
+++ src/main/java/net/brydget/harmony/util/MessageColorFormatter.java
@@ -3,6 +3,7 @@ package net.brydget.harmony.util;
 import net.brydget.harmony.BackendPlugin;
 import org.bukkit.ChatColor;
 
+import java.awt.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -27,8 +28,8 @@ public abstract class MessageColorFormatter {
                 message = message.replace(
                         hex,
                         "&x" + Arrays.stream(hex.substring(1)
-                                .split(""))
-                                .map(a -> "&"+a).collect(Collectors.joining(""))
+                                        .split(""))
+                                .map(a -> "&" + a).collect(Collectors.joining(""))
                 );
             }
         }
@@ -45,6 +46,114 @@ public abstract class MessageColorFormatter {
 
     public static String colorize(List<String> message) {
         return colorize(String.join("\n", message));
+    }
+
+    // Code from
+    // https://www.spigotmc.org/threads/gradient-chat-particles.470496/
+
+    static double[] interpolate(double from, double to, int max) {
+        final double[] res = new double[max];
+        for (int i = 0; i < max; i++) {
+            res[i] = from + i * ((to - from) / (max - 1));
+        }
+        return res;
+    }
+
+    /**
+     * Interpolates a RGB gradient onto the input string using the given colors.
+     *
+     * @param  str  the input string to colorize
+     * @param  from the starting color
+     * @param  to   the ending color
+     * @return      the colorized string
+     */
+    public static String rgbGradient(String str, Color from, Color to) {
+
+        // interpolate each component separately
+        final double[] red = interpolate(from.getRed(), to.getRed(), str.length());
+        final double[] green = interpolate(from.getGreen(), to.getGreen(), str.length());
+        final double[] blue = interpolate(from.getBlue(), to.getBlue(), str.length());
+
+        final StringBuilder builder = new StringBuilder();
+
+        // create a string that matches the input-string but has
+        // the different color applied to each char
+        for (int i = 0; i < str.length(); i++) {
+            builder.append(colorize(rgbToHex(
+                            (int) Math.round(red[i]),
+                            (int) Math.round(green[i]),
+                            (int) Math.round(blue[i]))))
+
+                    .append(str.charAt(i));
+        }
+
+        return builder.toString();
+    }
+
+    /**
+     * Returns a string after applying a gradient from one color to another.
+     *
+     * @param  str   the input string
+     * @param  from  the starting color of the gradient
+     * @param  to    the ending color of the gradient
+     * @return       the string after applying the color gradient
+     */
+    public static String gradient(String str, Color from, Color to) {
+        return rgbGradient(str, from, to);
+    }
+
+    /**
+     * Generates a gradient color string from the given input string using the
+     * specified start and end colors.
+     *
+     * @param  str  the input string
+     * @param  from the start color in hex format
+     * @param  to   the end color in hex format
+     * @return      the gradient color string
+     */
+    public static String gradient(String str, String from, String to) {
+        return rgbGradient(str, getColorFromHex(from), getColorFromHex(to));
+    }
+
+    /**
+     * colorize the given string from one color to another.
+     *
+     * @param  str  the string to colorize
+     * @param  from the starting color
+     * @param  to   the target color
+     * @return      the colorized string
+     */
+    public static String colorize(String str, Color from, Color to) {
+        return rgbGradient(str, from, to);
+    }
+
+    /**
+     * Colorizes the given string from one color to another.
+     *
+     * @param  str  the input string to be colorized
+     * @param  from the initial color in hex format
+     * @param  to   the target color in hex format
+     * @return      the colorized string
+     */
+    public static String colorize(String str, String from, String to) {
+        return rgbGradient(str, getColorFromHex(from), getColorFromHex(to));
+    }
+
+    /**
+     * Retrieves a Color object from a given hexadecimal string.
+     *
+     * @param  hex  the hexadecimal string representing the color
+     * @return      the Color object created from the hexadecimal string
+     */
+    public static Color getColorFromHex(String hex) {
+        int red = Integer.parseInt(hex.substring(1, 3), 16);
+        int green = Integer.parseInt(hex.substring(3, 5), 16);
+        int blue = Integer.parseInt(hex.substring(5, 7), 16);
+        return new Color(red, green, blue);
+    }
+
+    static String rgbToHex(int red, int green, int blue) {
+        return String.format("#%02x%02x%02x", red, green, blue);
     }
 
 }
